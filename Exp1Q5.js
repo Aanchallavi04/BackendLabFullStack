76130a93eb2a3b0303c2abb221d323a0c0d2ef37
Aanchal Agarwal <agarwalaanchal2005@gmail.com>
2025-08-29 10:01:14 +0530

    Update Exp1Q5.js

--- Exp1Q5.js
+++ Exp1Q5.js
@@ -9,12 +9,12 @@ const writeStream = fs.createWriteStream(filePath, { flags: "w", encoding: "utf8
 
 // Handle 'finish' event (when writing is done)
 writeStream.on("finish", () => {
-  console.log("✅ Data written successfully to output.txt");
+  console.log(" Data written successfully to output.txt");
 });
 
 // Handle errors
 writeStream.on("error", (err) => {
-  console.error("❌ Error writing file:", err.message);
+  console.error(" Error writing file:", err.message);
 });
 
 // Write data
